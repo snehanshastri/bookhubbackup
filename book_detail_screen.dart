26b0c7f2cfc29b2a60421b7c1b609d4d5a36d6e3
SNEHA N SHASTRI <snehashastri4002@gmail.com>
2024-07-09 21:00:44 +0530

    Update book_detail_screen.dart

--- book_detail_screen.dart
+++ book_detail_screen.dart
@@ -1,102 +1,133 @@
+import 'package:bookhubapp/cart.dart';
+import 'package:bookhubapp/models/audio_book.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:bookhubapp/auth_service.dart';
 import 'package:bookhubapp/models/books.dart';
+import 'package:bookhubapp/widgets/cartcontroller.dart';
 
 class BookDetailScreen extends StatelessWidget {
   final Book book;
+  final CartController cartController = Get.put(CartController());
 
-  const BookDetailScreen({super.key, required this.book});
+  BookDetailScreen({
+    Key? key,
+    required this.book,
+    required bool isLoggedIn,
+    required Null Function(List<Book> books, List<AudioBook> audioBooks) updateCart,
+  }) : super(key: key);
+
+  void navigateToLoginPage(BuildContext context) {
+    Get.toNamed('/login');
+  }
+
+  void addToCart(BuildContext context) {
+    if (AuthService.isLoggedIn()) {
+      if (cartController.isBookInCart(book)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('${book.title} is already in the cart'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      } else {
+        cartController.addToCart(book);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('${book.title} added to cart'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      }
+    } else {
+      navigateToLoginPage(context);
+    }
+  }
+
+  void viewCart(BuildContext context) {
+    Get.to(() => CartPage()); // Navigate to CartPage
+  }
 
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
           book.title,
-          style: const TextStyle(color: Colors.black),
+          style: TextStyle(color: Colors.black),
         ),
         backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(color: Colors.black),
+        iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image.network(
-                book.imageUrl,
-                height: screenWidth * 0.5,
-                width: screenWidth * 0.4,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.network(
+                  book.imageUrl,
+                  height: screenWidth * 0.5,
+                  width: screenWidth * 0.4,
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              book.title,
-              style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: Colors.black),
-            ),
-            Text(
-              book.genre,
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.black),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              book.description,
-              style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.black),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Price: ${book.price}',
-              style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.black),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Rating: ${book.rating}',
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.black),
-            ),
-            const Spacer(),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Implement Buy Now functionality
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
+              SizedBox(height: 16),
+              Text(
+                book.title,
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.black),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Price: ₹ ${book.price}',
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+              SizedBox(height: 16),
+              Text(
+                book.description,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+              SizedBox(height: 16),
+               Text(
+                'Rating:  ${book.rating}',
+                style: TextStyle(fontSize: 18, color: Colors.black),
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        addToCart(context);
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16.0),
+                        child: Text('Add to Cart', style: TextStyle(fontSize: 18)),
                       ),
                     ),
-                    child: const Text(
-                      'Buy Now',
-                      style: TextStyle(color: Colors.white),
-                    ),
                   ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Implement Add to Cart functionality
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        viewCart(context);
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16.0),
+                        child: Text('View Cart', style: TextStyle(fontSize: 18)),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
                       ),
                     ),
-                    child: const Text(
-                      'Add to Cart',
-                      style: TextStyle(color: Colors.white),
-                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
