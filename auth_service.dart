38cfbca8a0be666b729986728a7139ce1810afce
SNEHA N SHASTRI <snehashastri4002@gmail.com>
2024-08-26 18:15:17 +0530

    Update auth_service.dart

--- auth_service.dart
+++ auth_service.dart
@@ -1,9 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:bookhubapp/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+
 
 class AuthService {
-  static final FirebaseAuth _auth = FirebaseAuth.instance;
+  static final firebase_auth.FirebaseAuth _auth = firebase_auth.FirebaseAuth.instance;
 
-  static User? get currentUser => _auth.currentUser;
+  static firebase_auth.User? get currentUser => _auth.currentUser;
+   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   static bool isLoggedIn() {
     return _auth.currentUser != null;
@@ -13,5 +17,16 @@ class AuthService {
     return _auth.currentUser?.uid ?? '';
   }
 
+Future<User?> fetchUserData(firebase_auth.User firebaseUser) async {
+    try {
+      DocumentSnapshot doc = await _firestore.collection('users').doc(firebaseUser.uid).get();
+      if (doc.exists) {
+        return User.fromJson(doc.data() as Map<String, dynamic>);
+      }
+    } catch (e) {
+      print("Error fetching user data: $e");
+    }
+    return null;
+  }
   // Add more authentication methods if needed
 }
